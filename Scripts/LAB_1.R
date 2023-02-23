bfce9c8d3816981ee7436795830275ce186e4ebd
xMarce20pacheco <pachecoximena260@gmail.com>
2023-02-23 08:47:42 -0600

    actualizar

--- Scripts/LAB_1.R
+++ Scripts/LAB_1.R
@@ -2,3 +2,41 @@
 #23/02/2023
 #LABORATORIO 1 
 
+#Gastos totales
+300 + 240 + 1527 + 400 + 1500 + 1833 
+
+celular<-300
+transporte<-240
+comestibles<-1527
+gimnasio<-400
+alquiler<-1500
+otros<-1833
+
+#gastos totales
+celular + transporte + comestibles + gimnasio + alquiler + otros
+
+#gastos por mes
+5800*5
+#gastos por año
+5800*10
+
+gastos <-c(celular,transporte,comestibles,gimnasio,alquiler,otros)
+
+gastos
+
+barplot(gastos)
+
+nomb <- c("celular", "transporte", "comestibles", "gimnasio", "alquiler", "otros")
+
+barplot(gastos, 
+        col=c("pink", "red", "green", "blue", "pink", "blue"), 
+        main="gastos universitarios por mes",
+        ylim=c(0,2000), xlab="rubros",
+        ylab="costos en mxn",
+        names.arg=c("celular", "transporte", "comestibles", "gimnasio", "alquiler", "otros"),
+        )
+
+
+
+
+
