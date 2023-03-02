6ee9ec5db697e2ff8aa93890bc511a3963a55a85
xMarce20pacheco <pachecoximena260@gmail.com>
2023-03-02 08:13:00 -0600

    base de datos profepa

---
+++ Scripts/profepa.R
@@ -0,0 +1,7 @@
+#XIMENA MARCELA PACHECO GOMEZ
+#02/03/23
+
+#laboratorio semana 3 
+
+prof_url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv"
+profepa <- read.csv(prof_url)
