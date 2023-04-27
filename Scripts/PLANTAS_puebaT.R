ff5ec30da45a2b9a41fd501aed54c6449e0116ad
xMarce20pacheco <pachecoximena260@gmail.com>
2023-04-27 10:16:11 -0600

    TERMINADO EN CLASE

--- Scripts/PLANTAS_puebaT.R
+++ Scripts/PLANTAS_puebaT.R
@@ -9,12 +9,37 @@
 
 ## obligar a que solo busque en esa carpeta
 setwd("C:/REPOSITORIO GIT/D_experimental/Scripts/")
-PLANTAS <- read.csv("Scripts/bd1_vivero.csv", header=TRUE)
+PLANTAS <- read.csv("bd1_vivero.csv", header=TRUE)
 
 ### Cambiar de character a factor
 
 PLANTAS$Tratamiento <- as.factor(PLANTAS$Tratamiento)
 
 # REVISAR DATOS -----------------------------------------------------------
+
+#NORMALIDAD
+shapiro.test(PLANTAS$IE)
+##  <son normales>
+
+
+#VARIANAZAS
+bartlett.test(PLANTAS$IE ~ PLANTAS$Tratamiento)
+
+
+# P_T INDEPENDIENTES ------------------------------------------------------
+
+t.test(PLANTAS$IE ~ PLANTAS$Tratamiento, var.equal=TRUE)
+## Rechazo la hipotesis nula, el fertilizante si esta haciendo que las plantas mejoren en IE
+
+
+
+# PRUEBA DE 1 MUESTRA -----------------------------------------------------
+mean(PLANTAS$IE)
+## Si existen diferencias sig con .90 (mean fert)
+
+t.test(PLANTAS$IE, mu = 0.90)
+
+##cambiar el valor de la media establecida 
+t.test(PLANTAS$IE, mu = 0.85)
 
 
